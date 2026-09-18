74a3ec0d2cc7dd760b3d50be4371c7cefa4d80c2
Bruk-Git <brukome@gmail.com>
2026-09-18 21:19:40 +0300

    Fix vite config for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: "terser",
-    sourcemap: false, // Don't expose source code
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          icons: ["react-icons"],
-        },
-      },
-    },
+    outDir: "dist",
+    sourcemap: false,
   },
 });
